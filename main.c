8b8da59728f804383d47550d5c8c5383d65b60b2
Andrey Yuskovets <yuskovets1997@gmail.com>
2023-10-26 00:32:00 +0300

    init commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * 1-blink.c
+ *
+ * Created: 26.10.2023 0:23:48
+ * Author : Andrey Yuskovets
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
