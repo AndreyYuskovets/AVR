8c916f0219fb767f97b48e94d40e23f1f15e0ff1
Andrey Yuskovets <yuskovets1997@gmail.com>
2023-10-26 01:59:12 +0300

    blink v1

--- main.c
+++ main.c
@@ -5,14 +5,20 @@
  * Author : Andrey Yuskovets
  */ 
 
+#define F_CPU 4800000U
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRB |= (1 << (PORTB4));
     while (1) 
     {
+		PORTB |= (1 << (PORTB4));
+		_delay_ms(300);
+		PORTB &= ~(1 << (PORTB4));
+		_delay_ms(300);
+		
     }
 }
 
